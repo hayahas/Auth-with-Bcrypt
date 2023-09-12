783884d7566491615d3bdc72992827828e962110
hayahas <103423497+hayahas@users.noreply.github.com>
2023-09-12 23:49:40 +0300

    initial commit from spring app

---
+++ BcryptProj/src/main/java/com/Ltuc/BcryptProj/BcryptProjApplication.java
@@ -0,0 +1,13 @@
+package com.Ltuc.BcryptProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcryptProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BcryptProjApplication.class, args);
+	}
+
+}
---
+++ BcryptProj/src/test/java/com/Ltuc/BcryptProj/BcryptProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Ltuc.BcryptProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcryptProjApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
